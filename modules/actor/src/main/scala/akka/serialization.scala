4974a23d95e15e7d4694800018bdbf4c3f786f1b
Andrei Mikhailov <amikhailov@evolution.com>
2025-07-22 15:35:45 +0200

    scalafmt

--- modules/actor/src/main/scala/akka/serialization.scala
+++ modules/actor/src/main/scala/akka/serialization.scala
@@ -4,7 +4,8 @@ package object serialization {
   type Serializer = org.apache.pekko.serialization.Serializer
   val Serializers: org.apache.pekko.serialization.Serializers.type =
     org.apache.pekko.serialization.Serializers
-  type SerializerWithStringManifest = org.apache.pekko.serialization.SerializerWithStringManifest
+  type SerializerWithStringManifest =
+    org.apache.pekko.serialization.SerializerWithStringManifest
   type BaseSerializer = org.apache.pekko.serialization.BaseSerializer
   val BaseSerializer: org.apache.pekko.serialization.BaseSerializer.type =
     org.apache.pekko.serialization.BaseSerializer
