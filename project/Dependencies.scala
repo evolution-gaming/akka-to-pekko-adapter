ba6b881a7f6f696f7210315ac0269bb90cb990c2
Mikhail Sokolov <msokolov@evolution.com>
2025-09-08 14:08:19 +0300

    Update Pekko to 1.2.0, prepare for 1.0.0 release
    * Pekko updated from 1.1.5 to 1.2.0 (latest)
    * Pekko-Http updated from 1.1.0 to 1.2.0 (latest)
    * Pekko-Management updated from 1.1.0 to 1.1.1 (latest)
    * updated scalafmt, sbt, sbt plugins, github actions
    * set up bincompat checks

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,10 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
   object Pekko {
-    val PekkoVersion = "1.1.5"
-    val PekkoHttpVersion = "1.1.0"
+    val PekkoVersion = "1.2.0"
+    val PekkoHttpVersion = "1.2.0"
+    val PekkoManagementVersion = "1.1.1"
     val actor = "org.apache.pekko" %% "pekko-actor" % PekkoVersion
     val persistence = "org.apache.pekko" %% "pekko-persistence" % PekkoVersion
     val cluster = "org.apache.pekko" %% "pekko-cluster" % PekkoVersion
@@ -11,11 +12,12 @@ object Dependencies {
     val sharding = "org.apache.pekko" %% "pekko-cluster-sharding" % PekkoVersion
     val http = "org.apache.pekko" %% "pekko-http" % PekkoHttpVersion
     val spray = "org.apache.pekko" %% "pekko-http-spray-json" % PekkoHttpVersion
-    val management = "org.apache.pekko" %% "pekko-management" % PekkoHttpVersion
+    val management =
+      "org.apache.pekko" %% "pekko-management" % PekkoManagementVersion
     val bootstrap =
-      "org.apache.pekko" %% "pekko-management-cluster-bootstrap" % PekkoHttpVersion
+      "org.apache.pekko" %% "pekko-management-cluster-bootstrap" % PekkoManagementVersion
     val `cluster-http` =
-      "org.apache.pekko" %% "pekko-management-cluster-http" % PekkoHttpVersion
+      "org.apache.pekko" %% "pekko-management-cluster-http" % PekkoManagementVersion
     val `test-kit` =
       "org.apache.pekko" %% "pekko-testkit" % PekkoVersion
   }
